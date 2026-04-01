f655e4f5903330d66e5559917f61c7b5e6bc5a2d
WessamElshatawy <w.e.elshatawy@xed.aucegypt.edu>
2026-04-01 21:44:34 +0200

    audio.js

--- audio.js
+++ audio.js
@@ -1,34 +1,46 @@
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-// unlock audio
 document.addEventListener('click', () => {
   if (audioCtx.state === 'suspended') {
     audioCtx.resume();
   }
 });
 
-function playTone(freq, duration) {
+let rhythm = [];
+let recording = false;
+let startTime = 0;
+
+function playTone(freq, duration, type="sine") {
   const osc = audioCtx.createOscillator();
   const gain = audioCtx.createGain();
 
+  osc.type = type;
   osc.frequency.value = freq;
+
   osc.connect(gain);
   gain.connect(audioCtx.destination);
 
   gain.gain.value = 0.3;
 
   osc.start();
-
-  setTimeout(() => {
-    osc.stop();
-  }, duration);
+  setTimeout(() => osc.stop(), duration);
 }
 
-// MAIN TAP FUNCTION
 function tap(type) {
+  let instrument = document.getElementById("instrument").value;
+
   if (type === "1") {
-    playTone(600, 200); // stressed
+    playTone(600, 200, instrument);
+    showTap(true);
   } else {
-    playTone(250, 120); // unstressed
+    playTone(250, 120, instrument);
+    showTap(false);
+  }
+
+  if (recording) {
+    rhythm.push({
+      type: type,
+      time: Date.now() - startTime
+    });
   }
 }
